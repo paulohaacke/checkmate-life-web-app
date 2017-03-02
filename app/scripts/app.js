d9f6ac68d8fdcb5aee83bd02b9a7c21488f9aef7
Paulo Haacke <paulohaacke@gmail.com>
2017-03-01 22:53:05 -0300

    Scaffolding angular application.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name checkmateLifeWebAppApp
+ * @description
+ * # checkmateLifeWebAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('checkmateLifeWebAppApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name checkmateLifeWebAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the checkmateLifeWebAppApp
+ */
+angular.module('checkmateLifeWebAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
