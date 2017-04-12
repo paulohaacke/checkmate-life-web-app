43a4f3732c41314584b998fc6efb309ea7d07963
Paulo Haacke <paulohaacke@gmail.com>
2017-04-12 00:39:24 -0300

    Fixing some things in authentication and dashboard.

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -37,6 +37,5 @@ angular.module('CheckmateLifeApp')
                 numOfTasks += goal.lifeArea == area._id && goal.metrics.length > 0 ? $scope.getMetricStatus(goal, goal.metrics[0]) : 0;
             });
             return numOfTasks;
-            return 0;
         }
     }]);
